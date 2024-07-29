97b5b2403ff66d981478ba1105af1cbc57686d9a
USER <USER@DESKTOP-EV2K69F>
2024-07-29 22:40:07 +0900

    07.29 코테 커밋!

--- gogogo/src/programmers/LV2/P154539.java
+++ gogogo/src/programmers/LV2/P154539.java
@@ -25,6 +25,7 @@ public class P154539 {
 		int answer[] = new int[numbers.length];
 		Arrays.fill(answer, -1);
 		
+		
 		Stack<Integer> st = new Stack<>();		
 		
 		for (int i = numbers.length-1; i>=0;i--) {
